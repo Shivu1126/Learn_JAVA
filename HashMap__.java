d3213bf5882fa4456f12f081fc49245b9af01c81
Shivu1126 <99593409+Shivu1126@users.noreply.github.com>
2022-05-09 11:10:48 +0530

    Add files via upload

--- HashMap__.java
+++ HashMap__.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-// a Singly Linked List
 public class HashMap__ {
 
 	map head; // head of list
@@ -20,83 +19,19 @@ public class HashMap__ {
 		}
 	}
 		
-	public static HashMap__ remove(HashMap__ list,int k)
-	{
-		//Store head
-		map currNode = list.head, prev = null;
-		
-		
-	
-		//If head node itself holds the key to be deleted
-		
-		if (currNode != null && currNode.key == k) 
-		{
-			list.head = currNode.next; // Changed head
 			
-			// Display the message
-			System.out.println(k + " found and deleted");
-			
-			
-			return list;
-		}
-		
-		
-		// If the key is somewhere other than at head
-		
-		// Search for the key to be deleted,
-		// keep track of the previous node
-		// as it is needed to change currNode.next
-		while (currNode != null && currNode.key != k)
-		{
-			// If currNode does not hold key
-			// continue to next node
-			prev = currNode;
-			currNode = currNode.next;
-		}
-		
-		// If the key was present, it should be at currNode
-		// Therefore the currNode shall not be null
-		if (currNode != null) 
-		{
-			// Since the key is at currNode
-			// Unlink currNode from linked list
-			prev.next = currNode.next;
-			
-			// Display the message
-			System.out.println(k + " found and deleted");
-		}
-				
-		// CASE 3: The key is not present
-
-		// If key was not present in linked list
-		// currNode should be null
-		if (currNode == null) 
-		{
-		// Display the message
-			System.out.println(k + " not found ");
-		}
-		
-		// return the List
-		return list;
-	}
-			
-	// Method to insert a new node
+	//insert
 	public static HashMap__ insert(HashMap__ l, int k,int v)
 	{
-		// Create a new node with given data
+		//create
 		map new_map = new map(k,v);
-		//new_map.next = null;
-
-		// If the List is empty,
-		// then make the new node as head
+		
 		if (l.head == null)
 		{
 			l.head = new_map;
 		}
 		else
 		{
-			// Else traverse till the last node
-			// and insert the new_node there
 			map temp = l.head;
 			while (temp.next != null)
 			{
@@ -108,34 +43,62 @@ public class HashMap__ {
 					return l;
 				}
 			}
-
-			// Insert the new_node at last node
 			temp.next = new_map;
 		}
 
-		// Return the list by head
+		// return the list 
 		return l;
 	}
+	
+	// remove
+	public static HashMap__ remove(HashMap__ list,int k)
+	{
+		//store head
+		map currNode = list.head;
+		map prev = null;
+		
+		//head		
+		if (currNode != null && currNode.key == k) 
+		{
+			list.head = currNode.next; // Changed head			
+			System.out.println(k + " found and deleted");
+			
+			return list;
+		}	
+		// somewhere
+		while (currNode != null && currNode.key != k)
+		{
+			prev = currNode;
+			currNode = currNode.next;
+		}		
+		if (currNode != null) 
+		{
+			prev.next = currNode.next;
+			System.out.println(k + " found and deleted");
+		}				
+		//not present
+		if (currNode == null) 
+		{
+			System.out.println(k + " not found ");
+		}		
+		// return the List
+		return list;
+	}
 
-	// Method to print the LinkedList.
+	//print list
 	public static void printList(HashMap__ list)
 	{
 		map currMap = list.head;
-	
 		System.out.print("List : ");
 	
-		// Traverse through the List
 		while (currMap != null)
-		{
-			// Print the data at current node
-			System.out.print(currMap.key+ " "+currMap.value+" ");
-	
-			// Go to next node
+		{			
+			System.out.println(currMap.key+ " "+currMap.value+" ");
 			currMap = currMap.next;
 		}
-	}
+	}	
 	
-	// Driver code
+	// main
 	public static void main(String[] args)
 	{
 		HashMap__ list = new HashMap__();
@@ -144,8 +107,8 @@ public class HashMap__ {
 		
 		System.out.println("Your options : ");
 		System.out.println("1 : Insert ");
-		System.out.println("2 : Display");
-		System.out.println("3 : Remove");
+		System.out.println("2 : Remove");
+		System.out.println("3 : Display");
 		
 		
 		int rep=1;
